a75cfa54c1499fc8f2ad02cf90d3644c70aaac88
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-21 02:09:32 +0200

    .

--- src/main.py
+++ src/main.py
@@ -2,23 +2,15 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(
         ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+            ft.Column(
+                controls=[ft.Text("Thank you"), ft.Button(text="Hello")],
+                expand=True,
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
         )
     )
 
