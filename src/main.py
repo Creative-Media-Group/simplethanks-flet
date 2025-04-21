a3db18781c2b1562e1fcc3f661c3109203283212
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-21 02:31:41 +0200

    .

--- src/main.py
+++ src/main.py
@@ -1,13 +1,51 @@
 import flet as ft
 
 
+def mybutton(text: str, on_click, width: int, disabled: bool = False):
+    return ft.ElevatedButton(
+        content=ft.Text(text, size=20),
+        height=50,
+        width=width,
+        on_click=on_click,
+        bgcolor={
+            ft.ControlState.DEFAULT: ft.Colors.RED,
+            ft.ControlState.PRESSED: ft.Colors.TRANSPARENT,
+        },
+        expand=True,
+        disabled=disabled,
+        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10)),
+    )
+
+
+def play():
+    pass
+
+
 def main(page: ft.Page):
+    def on_resized(e):
+        page.update()
+
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.on_resized = on_resized
     page.add(
         ft.SafeArea(
             ft.Column(
-                controls=[ft.Text("Thank you"), ft.Button(text="Hello")],
+                controls=[
+                    ft.Row(
+                        controls=[ft.Text("Thank you")],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+                    ft.Row(
+                        controls=[
+                            mybutton(
+                                text="Hello",
+                                on_click=lambda _: play(),
+                                width=page.width * 0.5,
+                            )
+                        ]
+                    ),
+                ],
                 expand=True,
                 alignment=ft.MainAxisAlignment.CENTER,
             )
